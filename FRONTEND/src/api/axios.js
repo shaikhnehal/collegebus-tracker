2647d33c4a83b1f197061c6669fdb92833efc1ff
Fazle Rab <149478570+shaikhnehal@users.noreply.github.com>
2026-01-22 12:40:43 +0530

    Update API base URL to production environment

--- FRONTEND/src/api/axios.js
+++ FRONTEND/src/api/axios.js
@@ -20,7 +20,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://collegebus-tracker.onrender.com/api",
+
 });
 
 // 🔐 Auto token attach
