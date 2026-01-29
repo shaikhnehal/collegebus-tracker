a79a24e02442a8d8879031aa3f57f4a1b818e966
Fazle Rab <149478570+shaikhnehal@users.noreply.github.com>
2026-01-29 15:02:34 +0530

    Update axios baseURL to use environment variable

--- FRONTEND/src/api/axios.js
+++ FRONTEND/src/api/axios.js
@@ -20,9 +20,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://collegebus-tracker.onrender.com/api",
-
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 // 🔐 Auto token attach
